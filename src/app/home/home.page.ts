8507fd34042ed78798aa409ca3b42855791529ed
Thailane Santos <116779272+thailane-hub@users.noreply.github.com>
2024-08-13 23:51:22 +0000

    criei um layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,49 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+perfil = {
+  foto: 'https://bcw-media.s3.ap-northeast-1.amazonaws.com/yce_ai_anime_topbanner_mb_01_a51ba67c67.jpg',
+  sobre: 'Estudante de Desenvolvimento de Sistema',
+  nome:'Amanda Brach',
+  profissao:'Programador',
+  instagran:'@amandabrach',
+  idioma:'Português',
+  cidade:'Cerquilho/SP',
+  tempo:'Desde Janeiro de 2024',
 
+  metricas:[
+      {
+        texto:'curtidas',
+        valor:'45k'
+      },
+      {
+        texto:'Seguidos',
+        valor:'45k'
+      },
+      {
+        texto:'Amigos',
+        valor:'45k'
+      }
+     
+  ],
+
+  // formacao: [
+  //   // criou um objeto dentro da array
+  //   {
+  //     ano_inicio: '2023',
+  //     ano_termino: '2024',
+  //     intituicao: 'Etec Sales Gomes',
+  //     curso: 'Desenvolvimento de sistema',
+  //   },
+
+  //   {
+  //     ano_inicio: '2013',
+  //     ano_termino: '2015',
+  //     instituicao: 'Faculdade ',
+  //     curso: 'Desenvolvimento de sistema',
+  //   }
+
+  // ]
+}
   constructor(){ }
 }
