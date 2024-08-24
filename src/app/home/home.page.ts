1b39e84e536080b5c5332b1e3a38ec57c04e580a
Thailane Santos <116779272+thailane-hub@users.noreply.github.com>
2024-08-24 01:50:25 +0000

    api-countries

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,34 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  paises: any;
+  isloading: boolean = false;
 
-  constructor(){ }
+  constructor(){
+
+    this.getPaisCode();
+   }
+
+   getPaisCode(){
+    this.isloading = true;
+    // feth ele busca url chamada
+      fetch('https://restcountries.com/v3.1/all?fields=name,ccn3,flags')
+      .then( dados => dados.json())
+      // converto em json
+      .then(dados => {
+        console.log(dados);
+        this.paises = dados;
+      })
+      .catch(erro => {
+        console.log(erro);
+      })
+      // falha de conexao,falha de servidor pra fazer o cath do erro que aconteceu
+      .finally( () => {
+        console.log('Processo Finalizado');
+        this.isloading = false;
+      })
+
+      
+   }
+
 }
