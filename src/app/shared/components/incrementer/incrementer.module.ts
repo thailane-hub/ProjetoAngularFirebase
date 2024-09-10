bc760485166ee894619e26f2c0ea4de678be0c74
Thailane Santos <116779272+thailane-hub@users.noreply.github.com>
2024-09-10 22:46:42 +0000

    contador

---
+++ src/app/shared/components/incrementer/incrementer.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IncrementerComponent } from './incrementer.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [
+    IncrementerComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+
+exports:[
+  IncrementerComponent
+]
+  
+})
+export class IncrementerModule { }
